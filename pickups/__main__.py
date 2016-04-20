bc28ea415756d20828370f1547cc9aba4f1d3f12
Ernie Hershey <ernie.hershey@10gen.com>
2016-04-20 12:43:15 -0400

    Support cookie file override for multiple sessions

--- pickups/__main__.py
+++ pickups/__main__.py
@@ -12,14 +12,16 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     logging.getLogger('hangups').setLevel(logging.WARNING)
     dirs = appdirs.AppDirs('hangups', 'hangups')
-    default_cookies_path = os.path.join(dirs.user_cache_dir, 'cookies.json')
-    cookies = hangups.auth.get_auth_stdin(default_cookies_path)
 
     parser = argparse.ArgumentParser(description='IRC Gateway for Hangouts')
     parser.add_argument('--address', help='bind address', default='127.0.0.1')
     parser.add_argument('--port', help='bind port', default=6667)
+    parser.add_argument('--cookie-filename', help='cookie filename', default='cookies.json')
     parser.add_argument('--ascii-smileys', action='store_true',
                         help='display smileys in ascii')
     args = parser.parse_args()
+
+    default_cookies_path = os.path.join(dirs.user_cache_dir, args.cookie_filename)
+    cookies = hangups.auth.get_auth_stdin(default_cookies_path)
 
     Server(cookies, args.ascii_smileys).run(args.address, args.port)
